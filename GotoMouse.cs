87b869972ab9e701fb45e2204a79250a33108235
Yuri Sobral <53959412+sobral00@users.noreply.github.com>
2019-10-13 16:21:44 -0300

    Create GotoMouse.cs

---
+++ GotoMouse.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections;
+
+public class GotoMouse : MonoBehaviour
+{
+    public Animator animator;
+    public Vector3 target = new Vector3();
+    public Vector3 direction = new Vector3();
+    public Vector3 position = new Vector3();
+    public float speed = 2.0f;
+
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+    void FixedUpdate()
+    {
+        position = gameObject.transform.position;
+        if (Input.GetMouseButtonDown(0))
+        {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.z = 0;
+        }
+        if (target != Vector3.zero && (target - position).magnitude >= .06)
+        {
+            direction = (target - position).normalized;
+            gameObject.transform.position += direction * speed * Time.deltaTime;
+            animator.SetFloat("Walk", 1);
+            animator.SetFloat("PositionX", direction.x);
+            animator.SetFloat("PositionY", direction.y);
+        }
+        else
+        {
+            direction = direction.normalized;
+            animator.SetFloat("Walk",-1);
+            animator.SetFloat("StopX", direction.x);
+            animator.SetFloat("StopY", direction.y);
+        }
+    }
+}
