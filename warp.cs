ec2e62fcb12057f2fde7f244d4e6f4b58e1ba69b
Yuri Sobral <53959412+sobral00@users.noreply.github.com>
2019-10-16 17:17:06 -0300

    Create warp.cs

---
+++ warp.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class warp : MonoBehaviour
+{
+    public GameObject Portal;
+    public GameObject Player;
+     
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            StartCoroutine(Teleport());
+        }
+    }
+
+    IEnumerator Teleport()
+    {
+        yield return new WaitForSeconds(2);
+        Player.transform.position = new Vector2(Portal.transform.position.x, Portal.transform.position.y);
+    }
+ }
+
